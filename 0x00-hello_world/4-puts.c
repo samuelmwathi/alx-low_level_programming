ccc1ec97de8f23793d29f9d7733561734e75ba92
samuelmwathi <seniormwathi@gmail.com>
2023-09-08 12:51:24 -0700

    puts fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char string[]="Programming is like building a multilingual puzzle\n";
-	puts(string);
-    return (0);
+puts("Programming is like building a multilingual puzzle\n");
+return 0;
 }
