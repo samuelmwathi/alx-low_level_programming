c982b19c3331a58ce6985ec74082397e7750c7d0
samuelmwathi <seniormwathi@gmail.com>
2023-09-09 11:03:16 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <ctype.h> 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char b = 'B';
+putchar(tolower(b));
+return (0);
+}	
