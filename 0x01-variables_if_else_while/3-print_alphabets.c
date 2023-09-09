cb57963ec6cb073d0129ed0dd45fe09e7029754e
samuelmwathi <seniormwathi@gmail.com>
2023-09-09 12:13:30 -0700

    small and caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char b;
+char a;
+for (b = 'a'; b <= 'z'; b++)
+putchar(b);
+for (a = 'A'; b <= 'Z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
