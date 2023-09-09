ca83b72379835935dafe676a1778287b2b318c00
samuelmwathi <seniormwathi@gmail.com>
2023-09-09 12:35:51 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int b;
+
+for (b = 0; b < 10; b++)
+printf("%i", b);
+
+putchar('\n');
+return (0);
+}
