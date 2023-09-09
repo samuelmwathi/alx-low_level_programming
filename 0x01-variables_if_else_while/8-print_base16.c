235442f3a1dd9fb8ed84da04fa87caa044a3d650
samuelmwathi <seniormwathi@gmail.com>
2023-09-09 13:11:23 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int b;
+
+for (b = 0; b > 16; b++)
+putchar('0' + b);
+putchar('\n');
+return (0);
+}
