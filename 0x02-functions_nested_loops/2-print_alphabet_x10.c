eb7cd669ade51bf082b8747af40418c3db65962d
samuelmwathi <seniormwathi@gmail.com>
2023-09-16 00:31:31 -0700

    alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times alphabets
+ * returns : void
+ */
+void print_alphabet_x10(void)
+{
+char a;
+int x;
+for (x = 1; x <= 10; x++)
+{
+for (a = 'a'; a <= 'z'; a++)
+_putchar(a);
+}
+_putchar('\n');
+}
