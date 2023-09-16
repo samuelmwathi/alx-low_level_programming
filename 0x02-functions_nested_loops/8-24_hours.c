4fb5cb5d0933e0abb040606c6a493fe10ce03649
samuelmwathi <seniormwathi@gmail.com>
2023-09-16 03:12:46 -0700

    8_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,9 +5,11 @@
 void jack_bauer(void)
 {
 int a, b, c, d;
-for (a = 0; a <= 2; a++)
+for (a = 0; a <= 9; a++)
 {
-for (b = 0; b <= 3; b++)
+for (b = 0; b <= 9; b++)
+{
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
 for (c = 0; c <= 5; c++)
 {
@@ -19,7 +21,7 @@ _putchar(':');
 _putchar(c + '0');
 _putchar(d + '0');
 _putchar('\n');
-
+}
 }
 }
 }
