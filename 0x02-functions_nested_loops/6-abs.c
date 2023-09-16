ac50307703aa772fbec0e98acf99ea43d4b599c9
samuelmwathi <seniormwathi@gmail.com>
2023-09-16 01:42:06 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _abs - tels if s char is lower case
+ * @c: the character to be checked
+ * Return: abs 0f c
+ */
+int _abs(int c)
+{
+if (c < 0)
+return ((c * -1));
+return (c);
+}
