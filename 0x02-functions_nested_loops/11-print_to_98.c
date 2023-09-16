49775cc7fad5f8c9bc910d106fdc7227f5778dc6
samuelmwathi <seniormwathi@gmail.com>
2023-09-16 09:43:50 -0700

    mesage

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * print_to_98 - prints up to 98
+ * @x: start at
+ */
+void print_to_98(int x)
+{
+if (x == 98)
+{
+printf("%i \n", x);
+
+}
+if (x > 98)
+{
+while (x >=98)
+{
+if (x == 98)
+{
+printf("%i\n", x);
+}
+else 
+{
+printf("%i, ", x);
+}
+x--;
+}
+}
+if (x < 98)
+{
+while (x <= 98)
+{
+if (x == 98)
+{
+printf("%i\n ", x);
+}
+else 
+{
+printf("%i, ", x);
+}
+x++;
+}
+}
+}
+
