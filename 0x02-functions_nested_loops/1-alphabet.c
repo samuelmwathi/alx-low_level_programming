bef911786033a94a7e18d6e417cdb864e4a88fc1
samuelmwathi <seniormwathi@gmail.com>
2023-09-16 00:03:03 -0700

    alphabets:

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * prints alphabets 
+ * return: void
+ */
+void print_alphabet(void)
+{
+char a;
+for(a = 'a';a <= 'z'; a++)
+_putchar(a);
+_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,5 @@
 #define HEDER_FILE
 
 int _putchar(char c);
-
+void print_alphabet(void);
 #endif
