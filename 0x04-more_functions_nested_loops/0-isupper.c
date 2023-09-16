c4ab601051e0123976549cc297510f406cd34605
samuelmwathi <seniormwathi@gmail.com>
2023-09-16 13:29:38 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,17 @@
+#include"main.h"
+
+/**
+ * _isupper -shows if a character is upper
+ * @c: character to print
+ *  Return: 1 on success 0mother wise
+ */
+
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
-
 }
 
 
