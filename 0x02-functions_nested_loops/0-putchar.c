7741208599506f61f9243fdba9c3096e698e20b1
samuelmwathi <seniormwathi@gmail.com>
2023-09-15 23:35:54 -0700

    main.h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,9 @@
 #include"main.h"
+
 /**
- *main entry point 
- *always returns 0; 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,5 +17,7 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);	
+
+return (0);
+
 }
