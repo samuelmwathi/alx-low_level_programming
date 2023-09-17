6c48c17fa740a02dfc269b701a501ddaf55b3139
samuelmwathi <seniormwathi@gmail.com>
2023-09-17 03:37:55 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,11 @@
-
+#include"main.h"
+/**
+ * print_numbers -  prints nums 0-9
+ */
+void print_numbers(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+_putchar(x);
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,6 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 int mul(int a, int b);
+int _putchar(char c);
